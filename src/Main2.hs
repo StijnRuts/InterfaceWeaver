b8cf660187af4442a162e891f5dff9151eeded08
Stijn Ruts <git@stijnruts.be>
2025-12-07 12:08:50 +0100

    Improve wiring

--- src/Main2.hs
+++ src/Main2.hs
@@ -6,6 +6,7 @@ import Control.Category (Category, (>>>))
 import qualified Control.Category as C
 import Control.Monad.Free
 import Data.Char (toUpper)
+import Data.Void
 
 --
 
@@ -16,65 +17,65 @@ data ChannelF i o next
 
 type Channel i o = Free (ChannelF i o)
 
+type Transducer i o = Channel i o ()
+
+type Producer o = Transducer Void o
+
+type Consumer i = Transducer i Void
+
 emit :: o -> Channel i o ()
 emit o = liftF $ Emit o ()
 
 await :: Channel i o i
 await = liftF $ Await id
 
-newtype Pipe i o = Pipe {runPipe :: [i] -> [o]}
-
-instance Category Pipe where
-  id = Pipe id
-  (Pipe g) . (Pipe f) = Pipe (g . f)
-
 data Wire i o where
-  PipeIn :: Channel i o () -> Wire i o
+  FromTransducer :: Transducer i o -> Wire i o
   Seqential :: Wire a b -> Wire b c -> Wire a c
   Parallel :: Wire a b -> Wire a c -> Wire a (b, c)
   Merge :: Wire a b -> Wire a c -> Wire a (Either b c)
 
 --
 
-producer :: Channel () Int ()
+producer :: Producer Int
 producer = do
   emit 1
   emit 2
   emit 3
 
-double :: Channel Int Int ()
+double :: Transducer Int Int
 double = do
   x <- await
   emit $ 2 * x
 
-showStr :: Channel Int String ()
+showStr :: Transducer Int String
 showStr = do
   x <- await
   emit $ show x
 
-collector :: Channel String String ()
+collector :: Transducer String String
 collector = do
   s <- await
   emit $ "Collected: " ++ s
 
 --
 
-runChannel :: Channel i o () -> Pipe i o
-runChannel (Pure ()) = Pipe (const [])
-runChannel (Free (Emit x next)) = let Pipe f = runChannel next in Pipe (\xs -> x : f xs)
-runChannel (Free (Await next')) = Pipe (\(x : xs) -> let Pipe f = runChannel (next' x) in f xs)
+runTransducer :: Transducer i o -> [i] -> [o]
+runTransducer (Pure ()) = const []
+runTransducer (Free (Emit x next)) = let f = runTransducer next in (\xs -> x : f xs)
+runTransducer (Free (Await next')) = (\(x : xs) -> let f = runTransducer (next' x) in f xs)
 
 runWire :: Wire i o -> [i] -> [o]
-runWire (PipeIn ch) is = runPipe (runChannel ch) is
+runWire (FromTransducer ch) is = runTransducer ch is
 runWire (Seqential w1 w2) is = runWire w2 (runWire w1 is)
 runWire (Parallel w1 w2) is = zip (runWire w1 is) (runWire w2 is)
 runWire (Merge w1 w2) is = map Left (runWire w1 is) ++ map Right (runWire w2 is)
 
-pipeline :: Wire () (Either Int String)
+pipeline :: Wire Void (Either Int String)
 pipeline =
   Seqential
-    (PipeIn producer)
-    (Merge (PipeIn double) (PipeIn showStr))
+    (FromTransducer producer)
+    (Merge (FromTransducer double) (FromTransducer showStr))
 
 main :: IO ()
 main = print $ runWire pipeline []
