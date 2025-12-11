1c20f3bb7f5b13d397baed8ecdfe7dbf9bed7b65
Stijn Ruts <git@stijnruts.be>
2025-12-11 15:05:17 +0100

    WIP Refactor Channel

--- src/AltMain.hs
+++ src/AltMain.hs
@@ -76,3 +76,150 @@ chain producer consumer = do
 main :: IO ()
 main = runM . runOutputLine $ (fibProducer `chain` logger)
 
+{-
+a function to split a producer to multiple consumers
+
+a function to combine multiple producers into one consumer
+  - (all the same type) get whatever the latest value is
+     (<>) Producer a -> Producer a -> Producer a
+     append = sum id id
+  - Foldable t => t (Producer a) -> Producer a
+  - or, get a Product type of the latest value of each
+     Producer a -> Producer b -> Producer (a, b)
+     LiftA2 (,)
+     liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+     product = liftA2
+     requires Monoid for events that have not fired yet?
+  - or, get an Sum type of the last value
+     Producer a -> Producer b -> Producer (Either a b)
+     either :: (a -> c) -> (b -> c) -> Either a b -> c
+     liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+     the exact type (a -> c) -> (b -> c) -> f a -> f b -> f c is known as liftA2 (either)
+     sum = (aToC eg Left <$> eventsA) <> (bToC eg Right <$> eventsB)
+
+Generalize Sum and Product:
+c -> ((a -> c -> c), (b -> c -> c))
+
+(a -> _ -> Left a, b -> _ -> Right b)
+(a -> (_,b) -> (a,b), b -> (a,_) -> (a,b))
+
+sum, product (and pipe?) are (Free) Applicatives !
+
+
+
+
+-- Example program: independent tasks combined applicatively
+program :: FreeAp Task (String, Int)
+program = (,) <$> fetch "http://example.com" <*> compute 42
+
+-- Interpreter: run tasks in parallel
+runTask :: Task a -> IO a
+runTask (Fetch url) = do
+  putStrLn $ "Fetching from " ++ url
+  pure ("<html>dummy content from " ++ url ++ "</html>")
+runTask (Compute n) = do
+  putStrLn $ "Computing square of " ++ show n
+  pure (n * n)
+
+-- Parallel runner using async
+runParallel :: FreeAp Task a -> IO a
+runParallel = runAp (async . runTask) >=> \as -> do
+  results <- mapConcurrently wait as
+  pure (sequenceA results)
+
+main :: IO ()
+main = do
+  result <- runParallel program
+  print result
+
+
+
+
+If you want parallelism inside Polysemy, you can:
+- Define an effect like Parallel :: [Sem r a] -> Sem r [a].
+- Write an interpreter that uses async or mapConcurrently to run those sub-computations.
+- Internally, you could model those sub-computations as a free applicative program if you want to guarantee independence.
+
+data Task m a where
+  Fetch   :: String -> Task m String
+  Compute :: Int -> Task m Int
+
+makeSem ''Task  -- Polysemy boilerplate
+
+-- Free Applicative program inside Polysemy
+program :: FreeAp (Task (Sem r)) (String, Int)
+program = (,) <$> liftAp (Fetch "http://example.com")
+              <*> liftAp (Compute 42)
+
+-- Interpreter: run in parallel using async
+runTaskParallel :: Member (Embed IO) r => Task (Sem r) a -> Sem r a
+runTaskParallel (Fetch url) = embed $ download url
+runTaskParallel (Compute n) = embed $ pure (n * n)
+
+- Polysemy defines the effect system (Task).
+- Free Applicative structures the program so tasks can be parallelized.
+- The interpreter (runTaskParallel) decides how to execute them.
+
+
+
+
+
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveFunctor #-}
+
+import Polysemy
+import Polysemy.Embed
+import Control.Applicative.Free
+import Control.Concurrent.Async
+
+-- Define a Polysemy effect
+data Task m a where
+  Fetch   :: String -> Task m String
+  Compute :: Int -> Task m Int
+  deriving Functor
+
+makeSem ''Task  -- generates fetch, compute smart constructors
+
+-- Lift into Free Applicative
+fetchFA :: String -> FreeAp (Task (Sem r)) String
+fetchFA url = liftAp (Fetch url)
+
+computeFA :: Int -> FreeAp (Task (Sem r)) Int
+computeFA n = liftAp (Compute n)
+
+-- Example program: independent tasks combined applicatively
+program :: FreeAp (Task (Sem r)) (String, Int)
+program = (,) <$> fetchFA "http://example.com" <*> computeFA 42
+
+-- Interpreter: run tasks in parallel using async
+runTaskParallel :: Member (Embed IO) r => Task (Sem r) a -> Sem r a
+runTaskParallel (Fetch url) = embed $ do
+  putStrLn $ "Fetching from " ++ url
+  pure ("<html>dummy content from " ++ url ++ "</html>")
+runTaskParallel (Compute n) = embed $ do
+  putStrLn $ "Computing square of " ++ show n
+  pure (n * n)
+
+-- Fold the Free Applicative into Polysemy
+runParallelFA :: Member (Embed IO) r => FreeAp (Task (Sem r)) a -> Sem r a
+runParallelFA fa = do
+  -- runAp collects async actions
+  asyncs <- runAp (embed . async . interpretTask) fa
+  results <- embed $ mapConcurrently wait asyncs
+  pure (sequenceA results)
+  where
+    interpretTask :: Task (Sem r) a -> IO a
+    interpretTask (Fetch url) = do
+      putStrLn $ "Fetching from " ++ url
+      pure ("<html>dummy content from " ++ url ++ "</html>")
+    interpretTask (Compute n) = do
+      putStrLn $ "Computing square of " ++ show n
+      pure (n * n)
+
+main :: IO ()
+main = do
+  result <- runM $ runParallelFA program
+  print result
+
+-}
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PatternSynonyms #-}
 
 module Main (main) where
 
@@ -13,118 +13,116 @@ import Data.Void
 import System.Random (randomRIO)
 import Witherable
 
---
+-- Type definitions
 
 data ChannelF i o next
-  = Output o next
-  | Input (i -> next)
+  = Input (i -> next)
+  | Output o next
   deriving (Functor)
 
 type ChannelT i o m a = FreeT (ChannelF i o) m a
 
-output :: (Monad m) => o -> ChannelT i o m ()
-output o = liftF $ Output o ()
+newtype Channel m a i o = Channel (ChannelT i o m a)
+
+type Producer m a o = Channel m a Void o
+
+type IOProducer o = Producer IO () o
+
+type Consumer m a i = Channel m a i Void
+
+type IOConsumer i = Consumer IO () i
+
+type Program m a = Channel m a Void Void
+
+type IOProgram = Program IO ()
 
 input :: (Monad m) => ChannelT i o m i
 input = liftF $ Input id
 
-data Graph m a i o
-  = Embed (ChannelT i o m a)
-  | forall x. Sequential (Graph m a i x) (Graph m a x o)
-  | Parallel (Graph m a i o) (Graph m a i o)
+output :: (Monad m) => o -> ChannelT i o m ()
+output o = liftF $ Output o ()
 
-instance Semigroup (Graph m a i o) where
-  (<>) :: Graph m a i o -> Graph m a i o -> Graph m a i o
-  (<>) = Parallel
+-- Internal synonyms for shorter code
 
-instance (Monad m) => Category (Graph m a) where
-  id :: Graph m a x x
-  id = Embed $ forever $ input >>= output
-  (.) :: Graph m a x o -> Graph m a i x -> Graph m a i o
-  (.) = flip Sequential
+pattern InputF next = Free (Input next)
 
-instance (Monad m) => Functor (Graph m a i) where
-  fmap :: (o -> o') -> Graph m a i o -> Graph m a i o'
+pattern OutputF o next = Free (Output o next)
+
+input' = pure . Free . Input
+
+output' = pure . Free . Output
+
+pure' = pure . Pure
+
+onChan f (Channel chan) = Channel $ f chan
+
+onChan2 f (Channel chan1) (Channel chan2) = Channel $ f chan1 chan2
+
+runChanT run chanT = FreeT $ bind run (runFreeT chanT)
+
+runChanT2 run chanT1 chanT2 = FreeT $ liftA2 run (runFreeT chanT1) (runFreeT chanT2)
+
+-- TODO concurency
+
+-- Typeclass instances
+
+instance Semigroup (Channel m a i o) where
+  (<>) :: Channel m a i o -> Channel m a i o -> Channel m a i o
+  lChan <> rChan = onChan2 (runChanT2 parallel) lChan rChan
+    where
+      parallel (InputF f1) (InputF f2) = input' $ \i -> runChanT2 parallel (f1 i) (f2 i)
+      parallel (OutputF o1 next1) (OutputF o2 next2) = output' o1 $ output' o2 $ runChanT2 parallel next1 next2
+      parallel (OutputF o next) right = output' o $ runChanT2 parallel next right
+      parallel left (OutputF o next) = output' o $ runChanT2 parallel left next
+      parallel left (Pure _) = left
+      parallel (Pure _) right = right
+
+instance (Monad m) => Category (Channel m a) where
+  id :: Channel m a x x
+  id = Channel $ forever $ input >>= output
+  (.) :: Channel m a x o -> Channel m a i x -> Channel m a i o
+  oChan . iChan = onChan2 (runChanT2 sequential) iChan oChan
+    where
+      sequential (OutputF x next) (InputF f) = runChanT2 sequential next (f x)
+      sequential left (OutputF o next) = output' o $ runChanT2 sequential left next
+      sequential (InputF f) right = input' $ \i -> runChanT2 sequential (f i) right
+      sequential (Pure _) (Pure a) = pure' a
+      sequential (Pure a) (InputF _) = pure' a
+      sequential (OutputF _ _) (Pure a) = pure' a
+
+instance (Monad m) => Functor (Channel m a i) where
+  fmap :: (o -> o') -> Channel m a i o -> Channel m a i o'
   fmap = rmap
 
-instance (Monad m) => Profunctor (Graph m a) where
-  dimap :: (i' -> i) -> (o -> o') -> Graph m a i o -> Graph m a i' o'
-  dimap fi fo (Sequential left right) = Sequential (lmap fi left) (rmap fo right)
-  dimap fi fo (Parallel left right) = Parallel (dimap fi fo left) (dimap fi fo right)
-  dimap fi fo (Embed channel) = Embed (transFreeT (dimapChan fi fo) channel)
+instance (Monad m) => Profunctor (Channel m a) where
+  dimap :: (i' -> i) -> (o -> o') -> Channel m a i o -> Channel m a i' o'
+  dimap fi fo (Channel chan) = Channel $ transFreeT go chan
     where
-      dimapChan :: (i' -> i) -> (o -> o') -> ChannelF i o next -> ChannelF i' o' next
-      dimapChan _ fo (Output o next) = Output (fo o) next
-      dimapChan fi _ (Input next) = Input (next . fi)
+      go (Input next) = Input (next . fi)
+      go (Output o next) = Output (fo o) next
 
-instance (Monad m) => Filterable (Graph m a i) where
-  mapMaybe :: (o -> Maybe o') -> Graph m a i o -> Graph m a i o'
-  mapMaybe p (Sequential left right) = Sequential left (mapMaybe p right)
-  mapMaybe p (Parallel left right) = Parallel (mapMaybe p left) (mapMaybe p right)
-  mapMaybe p (Embed channel) = Embed (mapMaybeChanT p channel)
+instance (Monad m) => Filterable (Channel m a i) where
+  mapMaybe :: (o -> Maybe o') -> Channel m a i o -> Channel m a i o'
+  mapMaybe p = onChan (runChanT go)
     where
-      mapMaybeChanT :: (o -> Maybe o') -> ChannelT i o m a -> ChannelT i o' m a
-      mapMaybeChanT p chan = FreeT $ do
-        chanF <- runFreeT chan
-        case chanF of
-          Pure a -> pure $ Pure a
-          Free (Input next) -> pure $ Free $ Input $ \i -> mapMaybeChanT p (next i)
-          Free (Output o next) ->
-            case p o of
-              Just o' -> pure $ Free $ Output o' $ mapMaybeChanT p next
-              Nothing -> runFreeT $ mapMaybeChanT p next
+      go (Pure a) = pure' a
+      go (InputF next) = input' $ runChanT go next
+      go (OutputF o next) = case p o of
+        Just o' -> output' o' $ runChanT go next
+        Nothing -> runChanT go next
 
---
+{-
+-- https://hackage.haskell.org/package/base-4.21.0.0/docs/Control-Arrow.html
 
-runChannel :: (Monad m) => (o -> m ()) -> m i -> ChannelT i o m a -> m a
-runChannel put get channel = go channel
-  where
-    go chan = do
-      step <- runFreeT chan
-      case step of
-        Pure a -> pure a
-        Free (Output o next) -> put o >> go next
-        Free (Input next) -> go . next =<< get
+(^>>) :: Arrow a => (b -> c) -> a c d -> a b d
+(>>^) :: Arrow a => a b c -> (c -> d) -> a b d
 
-runChannel_ :: (Monad m) => ChannelT Void Void m a -> m a
-runChannel_ = runChannel (error "Can't output Void") (error "Can't input Void")
+(>|>) :: Filterable f => f a -> (a -> Bool) -> f a
+(>|>) = flip filter
 
-runGraph :: (Monad m) => (o -> m ()) -> m i -> Graph m a i o -> m a
-runGraph put get = runChannel put get . flattenGraph
-
-runGraph_ :: (Monad m) => Graph m a Void Void -> m a
-runGraph_ = runChannel_ . flattenGraph
-
-flattenGraph :: (Monad m) => Graph m a i o -> ChannelT i o m a
-flattenGraph (Embed channel) = channel
-flattenGraph (Sequential left right) = sequential (flattenGraph left) (flattenGraph right)
-flattenGraph (Parallel left right) = parallel (flattenGraph left) (flattenGraph right)
-
-sequential :: (Monad m) => ChannelT i x m a -> ChannelT x o m a -> ChannelT i o m a
-sequential lChan rChan = FreeT $ do
-  lChanF <- runFreeT lChan
-  rChanF <- runFreeT rChan
-  case (lChanF, rChanF) of
-    (Free (Output x next), Free (Input f)) -> runFreeT $ sequential next (f x)
-    (left, Free (Output o next)) -> pure $ Free $ Output o $ sequential (FreeT $ pure left) next
-    (Free (Input f), right) -> pure $ Free $ Input $ \i -> sequential (f i) (FreeT $ pure right)
-    ((Pure _), (Pure a)) -> pure (Pure a)
-    ((Pure a), (Free (Input _))) -> pure (Pure a)
-    ((Free (Output _ _)), (Pure a)) -> pure (Pure a)
-
-parallel :: (Monad m) => ChannelT i o m a -> ChannelT i o m a -> ChannelT i o m a
-parallel lChan rChan = FreeT $ do
-  lChanF <- runFreeT lChan
-  rChanF <- runFreeT rChan
-  case (lChanF, rChanF) of
-    (Free (Input f1), Free (Input f2)) -> pure $ Free $ Input $ \i -> parallel (f1 i) (f2 i)
-    (Free (Output o1 next1), Free (Output o2 next2)) -> pure $ Free $ Output o1 $ FreeT $ pure $ Free $ Output o2 $ parallel next1 next2
-    (Free (Output o next), right) -> pure $ Free $ Output o $ parallel next (FreeT $ pure right)
-    (left, Free (Output o next)) -> pure $ Free $ Output o $ parallel (FreeT $ pure left) next
-    (left, Pure _) -> pure left
-    (Pure _, right) -> pure right
-
---
+(>>|) :: Filterable f => f a -> (a -> Maybe b) -> f b
+(>>|) = flip mapMaybe
+-}
 
 {-
 data Merge s a b o = Merge
@@ -162,10 +160,21 @@ monoidMerge :: (Monoid a, Monoid b) => Merge (a, b) a b (a, b)
 monoidMerge = productMerge mempty mempty
 -}
 
---
+-- Runners
 
-fiveProducer :: Graph IO () Void Int
-fiveProducer = Embed $ do
+runChannel :: (Monad m) => m i -> (o -> m ()) -> Channel m a i o -> m a
+runChannel get put (Channel chan) = iterT go chan
+  where
+    go (Input next) = get >>= next
+    go (Output o next) = put o >> next
+
+runProgram :: (Monad m) => Program m a -> m a
+runProgram = runChannel (error "Can't input Void") (error "Can't output Void")
+
+-- Main program
+
+fiveProducer :: IOProducer Int
+fiveProducer = Channel $ do
   output 1
   lift $ threadDelay 1000000
   output 2
@@ -176,24 +185,27 @@ fiveProducer = Embed $ do
   lift $ threadDelay 1000000
   output 5
 
-fibProducer :: Graph IO () Void Int
-fibProducer = Embed $ go 0 1
+fibProducer :: IOProducer Int
+fibProducer = Channel $ go 0 1
   where
     go a b = do
       output a
       lift $ threadDelay 1000000
       go b (a + b)
 
-randomProducer :: Graph IO () Void Int
-randomProducer = Embed $ forever $ do
+randomProducer :: IOProducer Int
+randomProducer = Channel $ forever $ do
   n <- lift $ randomRIO (0, 999)
   output n
   lift $ threadDelay 1000000
 
-printer :: (Show s) => Graph IO () s Void
-printer = Embed $ forever $ do
+printer :: (Show s) => IOConsumer s
+printer = Channel $ forever $ do
   s <- input
   lift $ print s
 
+runner :: IOConsumer (IO ())
+runner = Channel $ forever $ lift =<< input
+
 main :: IO ()
-main = runGraph_ $ fibProducer >>> printer
+main = runProgram $ fibProducer >>> printer
